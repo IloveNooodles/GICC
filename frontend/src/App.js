22dc8bd7c447aff899173c3c70acc3f2eeb949ad
samuelswandi <13520075@std.stei.itb.ac.id>
2021-11-21 23:05:46 +0700

    Make new components, and early about-us page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,16 @@
 import "./App.css";
 import Navbar from "./components/navbar";
+import GICC from "./components/gicc";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-
-      <div className="top-border"></div>
-      <div className="GICC">
-        Ganesha <br></br>
-        Integration<br></br>
-        Case Competition <br></br>
+      <div className="first-section-container">
+        <Navbar />
+        <GICC />
       </div>
-        <p className="description-GICC">
-        We empower youth through professional cooperation experience in <br></br> solving industrial problems
-        </p>
-      <div className="bottom-border"></div>
-      
+
+      <div className="about-us"></div>
     </div>
   );
 }
---
+++ frontend/src/components/gicc.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./gicc.css"
+
+function gicc() {
+  return (
+    <div className="container">
+      <div className="top-border"></div>
+      <div className="GICC">
+        Ganesha <br></br>
+        <div className="left-circle"></div>
+        Integration<br></br>
+        <div className="right-circle"></div>
+        Case Competition <br></br>
+      </div>
+      <p className="description-GICC">
+        We empower youth through professional cooperation experience in
+        <br></br> solving industrial problems
+      </p>
+      <div className="bottom-border"></div>
+    </div>
+  );
+}
+
+export default gicc;
