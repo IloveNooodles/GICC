b780143d784cab64c5f4d783a82c932ffa5ec24f
samuelswandi <samuel.swandi@gmail.com>
2021-12-27 17:35:48 +0700

    Rework register page

--- frontend/src/components/registerLeader.js
+++ frontend/src/components/registerLeader.js
@@ -10,31 +10,53 @@ const registerLeader = () => {
         <Navbar />
         <div className="register-leader-title">Register as Team Leader</div>
       </div>
+
       <div className="register-leader-form">
         <div className="register-leader-card">
-          Address <br></br>
-          <input className="address-input"></input>
+          Email Address <br></br>
+          <input
+            className="email-input"
+            placeholder="GICC2022@gmail.com"
+          ></input>
         </div>
+
         <div className="register-leader-card">
-          Date of Birth <br></br>
-          <input className="dob-input"></input>
+          Name <br></br>
+          <input className="name-input" placeholder="Jane Doe"></input>
         </div>
+
         <div className="register-leader-card">
-          Gender <br></br>
-          <div className="gender-container">
-            <input className="male-input" type={"checkbox"}></input> Male
-            <input className="female-input" type={"checkbox"}></input> Female
-          </div>
+          No. Handphone <br></br>
+          <input className="handphone-input" placeholder="081234xxxx"></input>
         </div>
+
         <div className="register-leader-card-2">
           <div className="institution-container">
             Institution <br></br>
-            <input className="institution-input"></input>
+            <input className="institution-input" placeholder="ITB"></input>
           </div>
           <div className="major-container">
             Major/Faculty <br></br>
-            <input className="major-input"></input>
+            <input className="major-input" placeholder="Informatics"></input>
           </div>
+        </div>
+
+        <div className="register-leader-card">
+          Password <br></br>
+          <input
+            type="password"
+            className="password-input"
+            placeholder="password"
+          ></input>
+        </div>
+
+        <div className="register-leader-card">
+          Confirm Password <br></br>
+          <input
+            type="password"
+            className="confirm-password-input"
+            placeholder="confirm password"
+          ></input>
         </div>
 
         <div className="register-leader-card">
@@ -50,13 +72,28 @@ const registerLeader = () => {
         </div>
 
         <div className="register-leader-card">
+          Scan of Student ID card <br></br>
+          <input type={"file"} className="student-id-input"></input>
+        </div>
+
+        <div className="register-leader-card">
+          Screenshot of twibbon <br></br>
+          <input type={"file"} className="twibbon-input"></input>
+        </div>
+
+        <div className="register-leader-card">
+          Payment receipt <br></br>
+          <input type={"file"} className="payment-receipt-input"></input>
+        </div>
+
+        <div className="register-leader-card">
           Team Name <br></br>
-          <input className="team-name-input"></input>
+          <input className="team-name-input" placeholder="team name"></input>
         </div>
 
         <div className="register-leader-card">
           Referral Code <br></br>
-          <input className="referral-code-input"></input>
+          <input className="referral-code-input" placeholder="XXXXXX"></input>
           <div className="referral-code-description">
             <div className="referral-code-icon">i</div>
             Referral code can obtained from a GICC Campus Ambassador, you can
--- frontend/src/components/registerMember.js
+++ frontend/src/components/registerMember.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import Navbar from './navbar'
-import "./registerMember.css"
+import React from "react";
+import Navbar from "./navbar";
+import "./registerMember.css";
 
 const registerMember = () => {
   return (
@@ -9,31 +9,53 @@ const registerMember = () => {
         <Navbar />
         <div className="register-member-title">Register as Team Member</div>
       </div>
+
       <div className="register-member-form">
         <div className="register-member-card">
-          Address <br></br>
-          <input className="address-input"></input>
+          Email Address <br></br>
+          <input
+            className="email-input"
+            placeholder="GICC2022@gmail.com"
+          ></input>
         </div>
+
         <div className="register-member-card">
-          Date of Birth <br></br>
-          <input className="dob-input"></input>
+          Name <br></br>
+          <input className="name-input" placeholder="Jane Doe"></input>
         </div>
+
         <div className="register-member-card">
-          Gender <br></br>
-          <div className="gender-container">
-            <input className="male-input" type={"checkbox"}></input> Male
-            <input className="female-input" type={"checkbox"}></input> Female
-          </div>
+          No. Handphone <br></br>
+          <input className="handphone-input" placeholder="081234xxxx"></input>
         </div>
+
         <div className="register-member-card-2">
           <div className="institution-container">
             Institution <br></br>
-            <input className="institution-input"></input>
+            <input className="institution-input" placeholder="ITB"></input>
           </div>
           <div className="major-container">
             Major/Faculty <br></br>
-            <input className="major-input"></input>
+            <input className="major-input" placeholder="Informatics"></input>
           </div>
+        </div>
+
+        <div className="register-member-card">
+          Password <br></br>
+          <input
+            type="password"
+            className="password-input"
+            placeholder="password"
+          ></input>
+        </div>
+
+        <div className="register-member-card">
+          Confirm Password <br></br>
+          <input
+            type="password"
+            className="confirm-password-input"
+            placeholder="confirm password"
+          ></input>
         </div>
 
         <div className="register-member-card">
@@ -49,13 +71,32 @@ const registerMember = () => {
         </div>
 
         <div className="register-member-card">
-          Team Name <br></br>
-          <input className="team-name-input"></input>
+          Scan of Student ID card <br></br>
+          <input type={"file"} className="student-id-input"></input>
+        </div>
+
+        <div className="register-member-card">
+          Screenshot of twibbon <br></br>
+          <input type={"file"} className="twibbon-input"></input>
+        </div>
+
+        <div className="register-member-card">
+          Payment receipt <br></br>
+          <input type={"file"} className="payment-receipt-input"></input>
+        </div>
+
+        <div className="register-member-card">
+          Team Code <br></br>
+          <input className="team-name-input" placeholder="team code"></input>
+          <div className="team-code-description">
+            <div className="team-code-icon">i</div>
+            Ask your team leader for your team's code
+          </div>
         </div>
 
         <div className="register-member-card">
           Referral Code <br></br>
-          <input className="referral-code-input"></input>
+          <input className="referral-code-input" placeholder="XXXXXX"></input>
           <div className="referral-code-description">
             <div className="referral-code-icon">i</div>
             Referral code can obtained from a GICC Campus Ambassador, you can
@@ -66,7 +107,7 @@ const registerMember = () => {
         <button className="submit-button">Register</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default registerMember
+export default registerMember;
