d3b5d57117e66acc24a558767a5568c9e6d7e5db
GedeAryaRP <71829426+gedearyarp@users.noreply.github.com>
2022-01-10 21:10:49 +0700

    anjay profile page

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -2,11 +2,14 @@ import "./ProfilePage.css";
 
 import Navbar from "../components/navbar";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 import { Link, useNavigate } from "react-router-dom";
 
 const ProfilePage = () => {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [dataProfile, setDataProfile] = useState({});
+  const URL = "https://salty-temple-74931.herokuapp.com/";
   const navigate = useNavigate();
   useEffect(() => {
     const token = localStorage.getItem("Token");
@@ -18,6 +21,16 @@ const ProfilePage = () => {
   const handleLogout = () => {
     localStorage.removeItem("Token");
   };
+
+
+  useEffect(() => {
+    axios.get(URL + "user/profile",{headers: {Authorization: localStorage.getItem("Token")}})
+      .then(res => {
+        setDataProfile(res.data.profile);
+        console.log(res.data.profile);
+      }, []);
+    console.log("halo")
+  }, []);
 
   return (
     <div className="profile-root">
@@ -52,40 +65,42 @@ const ProfilePage = () => {
             Details : Coming soon
           </p>
         </div>
+        {/* email: "mgarebaldhie80@gmail.com"
+faculty: "Informatics"
+fullName: "Gare"
+institution: "ITB"
+lineID: null
+phoneNumber: "082216612992" */}
         <div className="right-profile-container">
           <h1>Profile</h1>
           <div className="right-text-content-profile">
             <div className="items-text-profile">
-              <p>Username</p>
-              <span>Username</span>
-            </div>
-            <div className="items-text-profile">
               <p>Full Name</p>
-              <span>Full Name</span>
+              { dataProfile.fullName ? (<span>{dataProfile.fullName}</span>) : (<span>-</span>) }
             </div>
             <div className="items-text-profile">
               <p>Faculty/Major</p>
-              <span>Username</span>
+              { dataProfile.faculty ? (<span>{dataProfile.faculty}</span>) : (<span>-</span>) }
             </div>
             <div className="items-text-profile">
               <p>Phone Number</p>
-              <span>Phone Number</span>
+              { dataProfile.phoneNumber ? (<span>{dataProfile.phoneNumber}</span>) : (<span>-</span>) }
             </div>
             <div className="items-text-profile">
               <p>Email</p>
-              <span>Email</span>
+              { dataProfile.email ? (<span>{dataProfile.email}</span>) : (<span>-</span>) }
             </div>
             <div className="items-text-profile">
               <p>Institution</p>
-              <span>Institution</span>
+              { dataProfile.institution ? (<span>{dataProfile.institution}</span>) : (<span>-</span>) }
             </div>
             <div className="items-text-profile">
               <p>ID Line</p>
-              <span>ID Line</span>
+              { dataProfile.lineID ? (<span>{dataProfile.lineID}</span>) : (<span>-</span>) }
             </div>
             <div className="items-text-profile">
               <p>Team Name</p>
-              <span>-</span>
+              { dataProfile.teamName ? (<span>{dataProfile.teamName}</span>) : (<span>-</span>) }
             </div>
           </div>
           <div className="right-buttons">
