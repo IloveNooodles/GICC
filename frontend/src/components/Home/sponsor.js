07ead21830f4c234e93cae4a9d0a12867f8ba466
samuelswandi <samuel.swandi@gmail.com>
2022-01-30 15:48:31 +0700

    feat(home-sponsor-section): finalizing section

--- frontend/src/components/Home/sponsor.js
+++ frontend/src/components/Home/sponsor.js
@@ -1,78 +1,143 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import Placeholder from "../assets/placeholder.png";
 import Rectangle1 from "../assets/Rectangle1.png"
 import Rectangle2 from "../assets/Rectangle2.png"
 import "./sponsor.css";
 
 const Sponsor = () => {
-  // const [sponsorList, setSponsorList] = useState([]);
-  // const [showSponsor, setShowSponsor] = useState(false);
+    const [sponsor1, setSponsor1] = useState(false);
+    const [sponsor2, setSponsor2] = useState(false);
+    const [sponsor3, setSponsor3] = useState(false);
+    const [sponsor4, setSponsor4] = useState(false);
 
-  // useEffect(() => {
-  //   const l = {url: Placeholder, text: "HAHAHA"}
-  //   setSponsorList(l);
-  // });
+    const showSponsor1 = () => {
+        if (sponsor1) {setSponsor1(false)}
+        else {setSponsor1(true)}
+        setSponsor2(false);
+        setSponsor3(false);
+        setSponsor4(false);
+    }
 
-  // const sponsorShow = (imageUrl, text) => {
-  //   const l2 = {url: imageUrl, text: text}
-  //   setSponsorList(l2);
-  //   setShowSponsor(true);
-  // }
+    const showSponsor2 = () => {
+        if (sponsor2) {setSponsor2(false)}
+        else {setSponsor2(true)}
+        setSponsor1(false);
+        setSponsor3(false);
+        setSponsor4(false);
+    }
 
-  return (
-    <div className="sponsor-container">
-      <div className="sponsor-title">Our Sponsors</div>
-      <div className="sponsor-card-container">
-        <div className="sponsor-card">
-          <img src={Placeholder} className="sponsor-exc-logo"></img>
-            <button className="sponsor-learn-more">Read More</button>
-        </div>
-        <div className="sponsor-card">
-          <img src={Placeholder} className="sponsor-exc-logo"></img>
-          <button className="sponsor-learn-more">Read More</button>
-        </div>
-        <div className="sponsor-card">
-          <img src={Placeholder} className="sponsor-exc-logo"></img>
-          <button className="sponsor-learn-more">Read More</button>
-        </div>
-        <div className="sponsor-card">
-          <img src={Placeholder} className="sponsor-exc-logo"></img>
-          <button className="sponsor-learn-more">Read More</button>
-        </div>
-        <div className="sponsor-card">
-          <img src={Placeholder} className="sponsor-exc-logo"></img>
-          <button className="sponsor-learn-more">Read More</button>
-        </div>
-        <div className="sponsor-card">
-          <img src={Placeholder} className="sponsor-exc-logo"></img>
-          <button className="sponsor-learn-more">Read More</button>
-        </div>
-        <div className="sponsor-card">
-          <img src={Placeholder} className="sponsor-exc-logo"></img>
-          <button className="sponsor-learn-more">Read More</button>
-        </div>
-        <div className="sponsor-card">
-          <img src={Placeholder} className="sponsor-exc-logo"></img>
-          <button className="sponsor-learn-more">Read More</button>
-        </div>
-        
-      </div>
+    const showSponsor3 = () => {
+        if (sponsor3) {setSponsor3(false)}
+        else {setSponsor3(true)}
+        setSponsor1(false);
+        setSponsor2(false);
+        setSponsor4(false);
+    }
 
-      
-      <div className="sponsor-page-body">
-        <div className="sponsor-page-title-aboutus">
-          <h1 data-aos="fade-right">
-            <img className="sponsor-logo-detailed" src={Placeholder} />
-          </h1>
+    const showSponsor4 = () => {
+        if (sponsor4) {setSponsor4(false)}
+        else {setSponsor4(true)}
+        setSponsor1(false);
+        setSponsor2(false);
+        setSponsor3(false);
+    }
+
+    return (<div className="sponsor-container">
+        <div className="sponsor-title">Our Sponsors</div>
+        <div className="sponsor-card-container">
+            <div className="sponsor-card">
+                <img alt="sponsor-logo" src={Placeholder} className="sponsor-exc-logo"/>
+                <button onClick={showSponsor1} className="sponsor-learn-more">Read More</button>
+            </div>
+            <div className="sponsor-card">
+                <img alt="sponsor-logo" src={Placeholder} className="sponsor-exc-logo"/>
+                <button onClick={showSponsor2} className="sponsor-learn-more">Read More</button>
+            </div>
+            <div className="sponsor-card">
+                <img alt="sponsor-logo" src={Placeholder} className="sponsor-exc-logo"/>
+                <button onClick={showSponsor3} className="sponsor-learn-more">Read More</button>
+            </div>
+            <div className="sponsor-card">
+                <img alt="sponsor-logo" src={Placeholder} className="sponsor-exc-logo"/>
+                <button onClick={showSponsor4} className="sponsor-learn-more">Read More</button>
+            </div>
         </div>
-        <div className="sponsor-page-text">
-          FJDKASLFJDSKALFJDSA
-        </div>
-      </div>
-     
-      
-    </div>
-  );
+
+
+        {sponsor1 ? <div className="sponsor-page-body">
+            <div className="sponsor-page-video">
+                <iframe className="sponsor-page-video-detailed"
+                        src="https://drive.google.com/file/d/1I1tf6Auj3CJ30krtLrzkqzP2K8EYJLQ_/preview"
+                        allow="autoplay"/>
+            </div>
+            <div className="sponsor-page-description">
+                <img src={Placeholder} alt="sponsor-logo" className="sponsor-page-logo"/>
+                <div className="sponsor-page-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac mollis turpis, eu sagittis
+                    diam. Duis nunc dolor, imperdiet at efficitur id, pretium sit amet justo. Sed est nisi,
+                    fermentum laoreet molestie sed, dignissim commodo eros. Morbi ac malesuada libero. Ut nec tellus
+                    cursus nunc volutpat dignissim. Mauris sagittis sit amet elit ac ornare. Vestibulum ornare
+                    semper convallis. Sed malesuada mauris ex, eu varius nunc fringilla vel. Praesent tincidunt
+                    tellus massa, in
+                </div>
+            </div>
+        </div> : <></>}
+
+        {sponsor2 ? <div className="sponsor-page-body">
+            <div className="sponsor-page-video">
+                <iframe className="sponsor-page-video-detailed"
+                        src="https://drive.google.com/file/d/1I1tf6Auj3CJ30krtLrzkqzP2K8EYJLQ_/preview"
+                        allow="autoplay"/>
+            </div>
+            <div className="sponsor-page-description">
+                <img src={Placeholder} alt="sponsor-logo" className="sponsor-page-logo"/>
+                <div className="sponsor-page-text">
+                    2Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac mollis turpis, eu sagittis diam.
+                    Duis nunc dolor, imperdiet at efficitur id, pretium sit amet justo. Sed est nisi, fermentum laoreet
+                    molestie sed, dignissim commodo eros. Morbi ac malesuada libero. Ut nec tellus cursus nunc volutpat
+                    dignissim. Mauris sagittis sit amet elit ac ornare. Vestibulum ornare semper convallis. Sed
+                    malesuada mauris ex, eu varius nunc fringilla vel. Praesent tincidunt tellus massa, in
+                </div>
+            </div>
+        </div> : <></>}
+
+        {sponsor3 ? <div className="sponsor-page-body">
+            <div className="sponsor-page-video">
+                <iframe className="sponsor-page-video-detailed"
+                        src="https://drive.google.com/file/d/1I1tf6Auj3CJ30krtLrzkqzP2K8EYJLQ_/preview"
+                        allow="autoplay"/>
+            </div>
+            <div className="sponsor-page-description">
+                <img src={Placeholder} alt="sponsor-logo" className="sponsor-page-logo"/>
+                <div className="sponsor-page-text">
+                    3Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac mollis turpis, eu sagittis diam.
+                    Duis nunc dolor, imperdiet at efficitur id, pretium sit amet justo. Sed est nisi, fermentum laoreet
+                    molestie sed, dignissim commodo eros. Morbi ac malesuada libero. Ut nec tellus cursus nunc volutpat
+                    dignissim. Mauris sagittis sit amet elit ac ornare. Vestibulum ornare semper convallis. Sed
+                    malesuada mauris ex, eu varius nunc fringilla vel. Praesent tincidunt tellus massa, in
+                </div>
+            </div>
+        </div> : <></>}
+
+
+        {sponsor4 ? <div className="sponsor-page-body">
+            <div className="sponsor-page-video">
+                <iframe className="sponsor-page-video-detailed"
+                        src="https://drive.google.com/file/d/1I1tf6Auj3CJ30krtLrzkqzP2K8EYJLQ_/preview"
+                        allow="autoplay"/>
+            </div>
+            <div className="sponsor-page-description">
+                <img src={Placeholder} alt="sponsor-logo" className="sponsor-page-logo"/>
+                <div className="sponsor-page-text">
+                    4Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac mollis turpis, eu sagittis diam.
+                    Duis nunc dolor, imperdiet at efficitur id, pretium sit amet justo. Sed est nisi, fermentum laoreet
+                    molestie sed, dignissim commodo eros. Morbi ac malesuada libero. Ut nec tellus cursus nunc volutpat
+                    dignissim. Mauris sagittis sit amet elit ac ornare. Vestibulum ornare semper convallis. Sed
+                    malesuada mauris ex, eu varius nunc fringilla vel. Praesent tincidunt tellus massa, in
+                </div>
+            </div>
+        </div> : <></>}
+    </div>);
 };
 
 export default Sponsor;
